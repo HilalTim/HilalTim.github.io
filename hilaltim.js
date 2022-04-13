ef3a9e0689ff87b20f8a1d2f2290db75e5ca04e3
HilalTim <100479968+HilalTim@users.noreply.github.com>
2022-04-13 10:18:23 +0300

    Update hilaltim.js

--- hilaltim.js
+++ hilaltim.js
@@ -94,7 +94,7 @@ $(document).ready( function(){
 $('#bg-image').backstretch([
 "https://www.imagevisit.com/images/2022/04/13/mustafa-kemal-ataturk-turkey-statue.jpg "
 	
-, "https://www.imagevisit.com/images/2022/04/13/28-288257_m.jpg "
+, "https://www.imagevisit.com/images/2022/04/13/Turk-Askeri-Resimler.jpg "
 	
 , "https://www.imagevisit.com/images/2022/04/13/thumb-1920-202113.jpg"
  
