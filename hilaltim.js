8edb33c1bd438a10e558bac58ff02648ea1048c1
HilalTim <100479968+HilalTim@users.noreply.github.com>
2025-01-14 02:28:44 +0300

    Update hilaltim.js

--- hilaltim.js
+++ hilaltim.js
@@ -92,7 +92,7 @@ $(document).ready( function(){
 $(document).ready( function(){
 
 $('#bg-image').backstretch([
-"https://www.imagevisit.com/images/2024/04/15/turkey-flag-ai-generated_724044-666.jpg "
+"https://cdn.imagevisit.com/2025/01/14/ddqu06p-25cf23a1-43c1-4e9b-874e-58224ae87f00.jpg"
 	
 , "https://www.imagevisit.com/images/2024/04/16/netlestirilmis.jpg "
 	
